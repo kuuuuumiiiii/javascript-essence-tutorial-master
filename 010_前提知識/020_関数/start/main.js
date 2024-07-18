3bb18876eceaf4fa96f50b0b9531e715678846b5
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2024-07-19 00:54:26 +0900

    実行環境

---
+++ 010_前提知識/020_関数/start/main.js
@@ -0,0 +1,8 @@
+// functionから始まるものが関数の定義となっている
+let name = "Tim"
+
+function hello(name) {
+  console.log("hello " + name);
+}
+
+hello(name);
---
+++ 020_実行環境/040_ホイスティング/start/main.js
@@ -0,0 +1,10 @@
+a();
+
+function a() {
+  console.log("a is called");
+}
+
+
+console.log(b);
+
+var b = 0;
